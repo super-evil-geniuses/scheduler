1433deadbc352cb5c5f1fc3940900c8c1b3eee54
sofiegraham <sofiegraham@hotmail.com>
2017-11-15 20:22:52 -0800

    Moved db syncing to db file

--- server/index.js
+++ server/index.js
@@ -9,11 +9,3 @@ app.use(express.static(__dirname + '/../client/dist/compiled'));
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`now listening on port ${PORT}`));
-
-// create tables in the database
-db.User.sync()
-db.Schedule.sync();
-db.Day_Part.sync();
-db.Actual_Schedule.sync();
-db.Employee_Availability.sync();
-db.Needed_Employee.sync();
