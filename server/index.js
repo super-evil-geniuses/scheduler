6855734be85622d13a5523f4c7b69feaf8b39352
sofiegraham <sofiegraham@hotmail.com>
2017-11-15 16:10:46 -0800

    React up and running

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static(__dirname + '/../client/dist/compiled'));
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`now listening on port ${PORT}`));
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist/compiled');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
@@ -16,7 +16,7 @@ module.exports = {
         loader : 'babel-loader',      
         query: {
           presets: ['react', 'es2015', 'stage-2']
-       }
+        }
       }
     ]
   }
