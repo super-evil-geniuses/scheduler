4d340f9e9f1eb2d36e806cd60d0e67d6c105e4bb
Caleb Choi <calebc1205@gmail.com>
2017-11-15 16:29:27 -0800

    set up db

---
+++ database/index.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+
+//Setting up postgres
+// 1. brew install postgres
+// 2. run "psql postgres"
+// checkout https://gist.github.com/apolloclark/ea5466d5929e63043dcf for commands
+// I had to manually create a database on my local machine using terminal
+// CREATE DATABASE <your database name> WITH OWNER <your owner name>
+// default password is "null"
+const sequelize = new Sequelize(/*database name*/, /*owner name*/, /*password*/, { host: 'localhost', dialect: 'postgres', });
+
+// underscored = (snake_case foreign keys)
+const User = sequelize.define('user', {
+	name: Sequelize.STRING,
+	role: Sequelize.STRING,
+	password: Sequelize.STRING,
+}, {underscored: true});
+
+const Schedule = sequelize.define('schedule', {
+	monday_dates: Sequelize.INTEGER,
+}, {underscored: true});
+
+const Employee_Availability = sequelize.define('employee_availability', {
+	is_available: Sequelize.BOOLEAN,
+}, {underscored: true});
+
+const Actual_Schedule = sequelize.define('actual_schedule', {
+	is_working: Sequelize.BOOLEAN,
+}, {underscored: true});
+
+const Needed_Employee = sequelize.define('needed_employee', {
+	employees_needed: Sequelize.INTEGER,
+}, {underscored: true});
+
+const Day_Part = sequelize.define('day_part', {
+	name: Sequelize.STRING,
+}, {underscored: true});
+
+
+// One-to-Many Relationships
+User.hasMany(Actual_Schedule, { as: 'actual_schedule'});
+User.hasMany(Employee_Availability, { as: 'employee_availability' });
+
+Schedule.hasMany(Actual_Schedule, { as: 'actual_schedule'});
+Schedule.hasMany(Needed_Employee, { as: 'needed_employee' });
+
+Day_Part.hasMany(Employee_Availability, { as: 'employee_availability' });
+Day_Part.hasMany(Actual_Schedule, { as: 'actual_schedule' });
+Day_Part.hasMany(Needed_Employee, { as: 'needed_employee' });
+
+module.exports = {
+	User: User,
+	Schedule: Schedule,
+	Employee_Availability: Employee_Availability,
+	Actual_Schedule: Actual_Schedule,
+	Needed_Employee: Needed_Employee,
+	Day_Part: Day_Part,
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+//my part
+const db = require('../database/index.js');
+
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist/compiled'));
@@ -7,3 +10,10 @@ app.use(express.static(__dirname + '/../client/dist/compiled'));
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`now listening on port ${PORT}`));
+
+db.User.sync()
+db.Schedule.sync();
+db.Day_Part.sync();
+db.Actual_Schedule.sync();
+db.Employee_Availability.sync();
+db.Needed_Employee.sync();
