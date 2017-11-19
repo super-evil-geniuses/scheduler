37ef5af2145d206116b50cacb79f0a2f95defd93
alfonso <alfonso@hansacanada.com>
2017-11-18 21:36:52 -0800

    adding setup for travis ci

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('../database/index.js');
+const bodyParser = require('body-parser');
 
 let app = express();
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist/compiled'));
 
 const PORT = process.env.PORT || 8080;
+
+app.post('/template-schedule', function (req, res) {
+  let mondayDate = req.body.mondayDate;
+  
+  //store the template
+
+  res.send(`week starting ${mondayDate} template is stored`);
+});
 
 app.listen(PORT, console.log(`now listening on port ${PORT}`));
