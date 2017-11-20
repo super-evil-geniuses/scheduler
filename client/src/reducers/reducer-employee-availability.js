537a08f8ce24e01125f02fe38036cc9fdc32705f
root <t.hoshman@gmail.com>
2017-11-20 14:09:22 -0800

    doing a commit because of unknown changes

---
+++ client/src/reducers/reducer-employee-availability.js
@@ -0,0 +1,45 @@
+const employeeAvail = [[
+  { name: 'Lucas', day_part: 'monA', is_available: true },
+  { name: 'Lucas', day_part: 'monP', is_available: true },
+  { name: 'Lucas', day_part: 'tuesA', is_available: true },
+  { name: 'Lucas', day_part: 'tuesP', is_available: false },
+  { name: 'Lucas', day_part: 'wedsA', is_available: true },
+  { name: 'Lucas', day_part: 'wedsP', is_available: true },
+  { name: 'Lucas', day_part: 'thursA', is_available: true },
+  { name: 'Lucas', day_part: 'thursP', is_available: false },
+  { name: 'Lucas', day_part: 'friA', is_available: true },
+  { name: 'Lucas', day_part: 'friP', is_available: true },
+  { name: 'Lucas', day_part: 'satA', is_available: true },
+  { name: 'Lucas', day_part: 'satP', is_available: true },
+  { name: 'Lucas', day_part: 'sunA', is_available: false },
+  { name: 'Lucas', day_part: 'sunP', is_available: false },
+],
+[
+  { name: 'Michael', day_part: 'monA', is_available: true },
+  { name: 'Michael', day_part: 'monP', is_available: true },
+  { name: 'Michael', day_part: 'tuesA', is_available: true },
+  { name: 'Michael', day_part: 'tuesP', is_available: true },
+  { name: 'Michael', day_part: 'wedsA', is_available: false },
+  { name: 'Michael', day_part: 'wedsP', is_available: false },
+  { name: 'Michael', day_part: 'thursA', is_available: false },
+  { name: 'Michael', day_part: 'thursP', is_available: false },
+  { name: 'Michael', day_part: 'friA', is_available: false },
+  { name: 'Michael', day_part: 'friP', is_available: false },
+  { name: 'Michael', day_part: 'satA', is_available: false },
+  { name: 'Michael', day_part: 'satP', is_available: false },
+  { name: 'Michael', day_part: 'sunA', is_available: true },
+  { name: 'Michael', day_part: 'sunP', is_available: true },
+]]
+
+
+const employeeAvailability = (state = employeeAvail, action) => {
+  switch (action.type) {
+    case 'UPDATE_EMPLOYEE_AVAILABILITY':
+      state[action.payload.id] = action.payload;
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default employeeAvailability;
