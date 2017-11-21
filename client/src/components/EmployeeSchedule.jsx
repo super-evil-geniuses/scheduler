a58333de3224d9faff87d4c5f763cf7aa1fe6e55
root <t.hoshman@gmail.com>
2017-11-21 10:01:34 -0800

    rendering schedule stuff

--- client/src/components/EmployeeSchedule.jsx
+++ client/src/components/EmployeeSchedule.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Off from './Off.jsx';
+import Shift from './Shift.jsx';
 
 const EmployeeSchedule = (props) => (
   <div>
-    {props.schedule.name}
+    <div>
+      {props.schedule.name}
+      <br></br>
+      {'Hours: ' + (props.schedule.schedule.length * 6)}
+    </div>
     <br></br>
-    {props.schedule.schedule}
+    {(() => {
+    	let shifts = [];
+  	  for (let i = 1; i < 15; i++) {
+  	  	if (props.schedule.schedule.indexOf(i) !== -1) {
+  	  		shifts.push(<Shift />)
+  	  	} else {
+  	  		shifts.push(<Off />)
+  	  	}
+  	  }
+      return shifts;
+    })()}
   </div>
 )
 
