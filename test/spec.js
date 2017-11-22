34b5d326d95d0ead5bc138182b12c303520f4279
alfonso <alfonso@hansacanada.com>
2017-11-22 11:19:26 -0800

    fixed up organization of the test spec and added some test cases for added employee

--- test/spec.js
+++ test/spec.js
@@ -12,55 +12,11 @@ const request = require('supertest'); //used for testing http
 //require the necessary files
 const app = require('../server/app.js');
 const schema = require('../database/config.js');
-const port = process.env.PORT || 8080;
 const algo = require('../helpers/algo.js');
+const utils = require('../helpers/index.js');
+const port = process.env.PORT || 8080;
 
-/************
-/* The following code needs to be updated to work with postgres. This was taken from a shortly express
-/************/
-// describe('', function() {
-//   var db;
-//   var server;
-
-//   var clearDB = function(connection, tablenames, done) {
-//     var count = 0;
-//     tablenames.forEach(function(tablename) {
-//       connection.query(`DROP TABLE IF EXISTS ${tablename}`, function() {
-//         count++;
-//         if (count === tablenames.length) {
-//           return schema(db).then(done); // CHANGE THIS LINE SINCE THE SETUP IS DIFFERENT THAN THE SPRINT'S DB 
-//         }
-//       });
-//     });
-//   };
-
-//   beforeEach(function(done) {
-
-//     db = new pg.Client({
-//       user: process.env.DB_USER || 'sofiegraham',
-//       password: process.env.DB_PASS || null,
-//       database: process.env.DB_NAME || 'shiftly'
-//     });
-
-//     ************************************************************************************
-//     /* TODO: If you create a new table, add it to the tablenames collection below. */
-//     /**************************************************************************************/
-//     var tablenames = ['user', 'schedule', 'employee_availability', 'actual_schedule', 'needed_employee', 'day_part'];
-
-//     db.connect(function(err) {
-//       if (err) { return done(err); }
-//       /* Empties the db table before each test so that multiple tests
-//        * (or repeated runs of the tests) won't screw each other up: */
-//       clearDB(db, tablenames, function() {
-//         server = app.listen(port, done);
-//       });
-//     });
-
-//     afterEach(function() { server.close(); });
-//   });
-// });
-
-describe('Shiftly Test Spec', function() {
+describe('Shiftly Test Spec', () => {
 
   let sequelize;
   let server;
@@ -90,150 +46,170 @@ describe('Shiftly Test Spec', function() {
     });
   });
 
-
-  // it('sent a template object and was successfully stored in the db', function(done) {
-  //   request(app)
-  //     .post('/template-schedule')
-  //     .set('Content-Type', 'application/json')
-  //     .send(JSON.stringify({
-  //       mondayDate: new Date('11/13/17'),
-  //       monA: 2,
-  //       monP: 2,
-  //       tuesA: 2,
-  //       tuesA: 2,
-  //       wedsA: 2,
-  //       wedsP: 2,
-  //       thursA: 2,
-  //       thursP: 2,
-  //       friA: 2,
-  //       friP: 2,
-  //       satA: 2,
-  //       satP: 2,
-  //       sunA: 2,
-  //       sunP: 2
-  //     }))
-  //     .expect(`template is stored`, done);
-  // });
-
-
-  it('should get a 200 response for getAllUsers request', function(done) {
-    request(app)
-      .get('/users')
-      .expect(200, done);
-  });
-});
-
-describe('Algo', function() {
-
-  let allEmployeeAvail = 
-{ '1': [ 1, 2, 5, 6, 7, 8, 9, 10 ],
-  '2': [ 1, 2, 5, 6, 7, 8, 10 ],
-  '3': [ 1, 2, 5, 6, 7, 8, 9, 10 ],
-  '4': [ 2, 5, 6, 7 ],
-  '5': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
-  '6': [ 1, 4, 6, 7, 8, 10 ],
-  '7': [ 1, 3, 5, 6, 7, 8, 9, 10 ],
-  '8': [ 5, 6, 10 ],
-  '9': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
-  '10': [ 1, 3, 4, 5, 6, 7, 8, 10 ],
-  '11': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
-  '12': [ 1, 3, 4, 5, 6, 8, 9, 10 ],
-  '13': [ 2, 4, 6, 7, 9 ],
-  '14': [ 2, 4, 5, 6, 7 ] };
-
-let temp = 
-{ '1': 1,
-  '2': 2,
-  '3': 2,
-  '4': 2,
-  '5': 2,
-  '6': 3,
-  '7': 2,
-  '8': 4,
-  '9': 3,
-  '10': 5,
-  '11': 4,
-  '12': 5,
-  '13': 3,
-  '14': 2 };
-
-  let result = algo.scheduleGenerator(allEmployeeAvail, temp);
-  it('algo should return an array', function(){
-    expect(result).to.be.a('object');
-  });
-
-  it('algo should schedule exactly as many servers as specified', function(){
-    expect(result['1']).to.have.lengthOf(temp['1']);
-    expect(result['2']).to.have.lengthOf(temp['2']);
-    expect(result['3']).to.have.lengthOf(temp['3']);
-    expect(result['4']).to.have.lengthOf(temp['4']);
-    expect(result['5']).to.have.lengthOf(temp['5']);
-    expect(result['6']).to.have.lengthOf(temp['6']);
-    expect(result['7']).to.have.lengthOf(temp['7']);
-    expect(result['8']).to.have.lengthOf(temp['8']);
-    expect(result['9']).to.have.lengthOf(temp['9']);
-    expect(result['10']).to.have.lengthOf(temp['10']);
-    expect(result['11']).to.have.lengthOf(temp['11']);
-    expect(result['12']).to.have.lengthOf(temp['12']);
-    expect(result['13']).to.have.lengthOf(temp['13']);
-    expect(result['14']).to.have.lengthOf(temp['14']);
-  });
-
-  it('algo should not schedule employees on days they aren\'t available', function () {
-    expect(result['4'].indexOf(1)).to.equal(-1);
-    expect(result['8'].indexOf(1)).to.equal(-1);
-    expect(result['13'].indexOf(1)).to.equal(-1);
-    expect(result['14'].indexOf(1)).to.equal(-1);
-
-    expect(result['5'].indexOf(2)).to.equal(-1);
-    expect(result['6'].indexOf(2)).to.equal(-1);
-    expect(result['7'].indexOf(2)).to.equal(-1);
-    expect(result['8'].indexOf(2)).to.equal(-1);
-    expect(result['9'].indexOf(2)).to.equal(-1);
-    expect(result['10'].indexOf(2)).to.equal(-1);
-    expect(result['11'].indexOf(2)).to.equal(-1);
-    expect(result['12'].indexOf(2)).to.equal(-1);
-
-    expect(result['1'].indexOf(3)).to.equal(-1);
-    expect(result['2'].indexOf(3)).to.equal(-1);
-    expect(result['3'].indexOf(3)).to.equal(-1);
-    expect(result['4'].indexOf(3)).to.equal(-1);
-    expect(result['6'].indexOf(3)).to.equal(-1);
-    expect(result['8'].indexOf(3)).to.equal(-1);
-    expect(result['13'].indexOf(3)).to.equal(-1);
-    expect(result['14'].indexOf(3)).to.equal(-1);
-
-    expect(result['1'].indexOf(4)).to.equal(-1);
-    expect(result['2'].indexOf(4)).to.equal(-1);
-    expect(result['3'].indexOf(4)).to.equal(-1);
-    expect(result['4'].indexOf(4)).to.equal(-1);
-    expect(result['7'].indexOf(4)).to.equal(-1);
-    expect(result['8'].indexOf(4)).to.equal(-1);
-
-    expect(result['6'].indexOf(5)).to.equal(-1);
-    expect(result['13'].indexOf(5)).to.equal(-1);
-
-    expect(result['8'].indexOf(7)).to.equal(-1);
-    expect(result['12'].indexOf(7)).to.equal(-1);
+  xdescribe('Server Routing:', () => {
+    it('should get 200 response with /add_employee endpoint', (done) => {
+      request(app)
+        .post('/add_employee')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'Al',
+        })
+        .expect(200, done);
+    });
     
-    expect(result['4'].indexOf(8)).to.equal(-1);
-    expect(result['8'].indexOf(8)).to.equal(-1);
-    expect(result['13'].indexOf(8)).to.equal(-1);
-    expect(result['14'].indexOf(8)).to.equal(-1);
+    it('adding a new employee creates a new user record', (done) => {
+      request(app)
+        .post('/add_employee')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'Alice',
+        })
+        .end((err, res) => {
+          if (err) { throw err; }
+          expect(res.body.name).to.equal('Alice');
+          done();
+        });
+    });
 
-    expect(result['2'].indexOf(9)).to.equal(-1);
-    expect(result['4'].indexOf(9)).to.equal(-1);
-    expect(result['6'].indexOf(9)).to.equal(-1);
-    expect(result['8'].indexOf(9)).to.equal(-1);
-    expect(result['10'].indexOf(9)).to.equal(-1);
-    expect(result['14'].indexOf(9)).to.equal(-1);
-
-    expect(result['4'].indexOf(10)).to.equal(-1);
-    expect(result['13'].indexOf(10)).to.equal(-1);
-    expect(result['14'].indexOf(10)).to.equal(-1);
+    it('should get a 200 response for getAllUsers request', function(done) {
+      request(app)
+        .get('/users')
+        .expect(200, done);
+    });
   });
 
-  it('algo should autofill house shift if not enough employees are available', function(){
-    expect(result['8'].indexOf('house')).to.not.equal(-1);
-  })
+  xdescribe('Middleware:', () => {
+    xit('adding a new employee creates the records in employee\'s availability table and sets all the availabilities to false', (done) => {
+      request(app)
+        .post('/add_employee')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'Alice',
+        })
+        .end((err, res) => {
+          if (err) { throw err; }
+          expect(res.body.name).to.equal('Alice');
+          done();
+        });
+    });
+
+  });
+
+  describe('Algo', () => {
+
+    let allEmployeeAvail = {
+      '1': [ 1, 2, 5, 6, 7, 8, 9, 10 ],
+      '2': [ 1, 2, 5, 6, 7, 8, 10 ],
+      '3': [ 1, 2, 5, 6, 7, 8, 9, 10 ],
+      '4': [ 2, 5, 6, 7 ],
+      '5': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
+      '6': [ 1, 4, 6, 7, 8, 10 ],
+      '7': [ 1, 3, 5, 6, 7, 8, 9, 10 ],
+      '8': [ 5, 6, 10 ],
+      '9': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
+      '10': [ 1, 3, 4, 5, 6, 7, 8, 10 ],
+      '11': [ 1, 3, 4, 5, 6, 7, 8, 9, 10 ],
+      '12': [ 1, 3, 4, 5, 6, 8, 9, 10 ],
+      '13': [ 2, 4, 6, 7, 9 ],
+      '14': [ 2, 4, 5, 6, 7 ], 
+    };
+
+    let temp = {
+      '1': 1,
+      '2': 2,
+      '3': 2,
+      '4': 2,
+      '5': 2,
+      '6': 3,
+      '7': 2,
+      '8': 4,
+      '9': 3,
+      '10': 5,
+      '11': 4,
+      '12': 5,
+      '13': 3,
+      '14': 2 
+    };
+
+    let result = algo.scheduleGenerator(allEmployeeAvail, temp);
+
+    it('algo should return an array', function(){
+      expect(result).to.be.a('object');
+    });
+
+    it('algo should schedule exactly as many servers as specified', function(){
+      expect(result['1']).to.have.lengthOf(temp['1']);
+      expect(result['2']).to.have.lengthOf(temp['2']);
+      expect(result['3']).to.have.lengthOf(temp['3']);
+      expect(result['4']).to.have.lengthOf(temp['4']);
+      expect(result['5']).to.have.lengthOf(temp['5']);
+      expect(result['6']).to.have.lengthOf(temp['6']);
+      expect(result['7']).to.have.lengthOf(temp['7']);
+      expect(result['8']).to.have.lengthOf(temp['8']);
+      expect(result['9']).to.have.lengthOf(temp['9']);
+      expect(result['10']).to.have.lengthOf(temp['10']);
+      expect(result['11']).to.have.lengthOf(temp['11']);
+      expect(result['12']).to.have.lengthOf(temp['12']);
+      expect(result['13']).to.have.lengthOf(temp['13']);
+      expect(result['14']).to.have.lengthOf(temp['14']);
+    });
+
+    it('algo should not schedule employees on days they aren\'t available', function () {
+      expect(result['4'].indexOf(1)).to.equal(-1);
+      expect(result['8'].indexOf(1)).to.equal(-1);
+      expect(result['13'].indexOf(1)).to.equal(-1);
+      expect(result['14'].indexOf(1)).to.equal(-1);
+
+      expect(result['5'].indexOf(2)).to.equal(-1);
+      expect(result['6'].indexOf(2)).to.equal(-1);
+      expect(result['7'].indexOf(2)).to.equal(-1);
+      expect(result['8'].indexOf(2)).to.equal(-1);
+      expect(result['9'].indexOf(2)).to.equal(-1);
+      expect(result['10'].indexOf(2)).to.equal(-1);
+      expect(result['11'].indexOf(2)).to.equal(-1);
+      expect(result['12'].indexOf(2)).to.equal(-1);
+
+      expect(result['1'].indexOf(3)).to.equal(-1);
+      expect(result['2'].indexOf(3)).to.equal(-1);
+      expect(result['3'].indexOf(3)).to.equal(-1);
+      expect(result['4'].indexOf(3)).to.equal(-1);
+      expect(result['6'].indexOf(3)).to.equal(-1);
+      expect(result['8'].indexOf(3)).to.equal(-1);
+      expect(result['13'].indexOf(3)).to.equal(-1);
+      expect(result['14'].indexOf(3)).to.equal(-1);
+
+      expect(result['1'].indexOf(4)).to.equal(-1);
+      expect(result['2'].indexOf(4)).to.equal(-1);
+      expect(result['3'].indexOf(4)).to.equal(-1);
+      expect(result['4'].indexOf(4)).to.equal(-1);
+      expect(result['7'].indexOf(4)).to.equal(-1);
+      expect(result['8'].indexOf(4)).to.equal(-1);
+
+      expect(result['6'].indexOf(5)).to.equal(-1);
+      expect(result['13'].indexOf(5)).to.equal(-1);
+
+      expect(result['8'].indexOf(7)).to.equal(-1);
+      expect(result['12'].indexOf(7)).to.equal(-1);
+      
+      expect(result['4'].indexOf(8)).to.equal(-1);
+      expect(result['8'].indexOf(8)).to.equal(-1);
+      expect(result['13'].indexOf(8)).to.equal(-1);
+      expect(result['14'].indexOf(8)).to.equal(-1);
+
+      expect(result['2'].indexOf(9)).to.equal(-1);
+      expect(result['4'].indexOf(9)).to.equal(-1);
+      expect(result['6'].indexOf(9)).to.equal(-1);
+      expect(result['8'].indexOf(9)).to.equal(-1);
+      expect(result['10'].indexOf(9)).to.equal(-1);
+      expect(result['14'].indexOf(9)).to.equal(-1);
+
+      expect(result['4'].indexOf(10)).to.equal(-1);
+      expect(result['13'].indexOf(10)).to.equal(-1);
+      expect(result['14'].indexOf(10)).to.equal(-1);
+    });
+
+    it('algo should autofill house shift if not enough employees are available', function(){
+      expect(result['8'].indexOf('house')).to.not.equal(-1);
+    })
+  });
 });
