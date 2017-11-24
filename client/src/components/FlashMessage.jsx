6f55756c0386079b53a4eaba88e8984103c798aa
root <t.hoshman@gmail.com>
2017-11-24 11:24:31 -0800

    brought over new files

---
+++ client/src/components/FlashMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FlashMessage = (props) => (
+  <div>
+  <h4>{props.message.toUpperCase()}</h4>
+  </div>
+	)
+
+
+export default FlashMessage;
---
+++ client/src/containers/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { changeView, login } from '../actions/index';
+
+const Login = (props) => (
+  <div className="employee-availability clear-fix">
+    <h4>Login</h4>
+    <form >
+        <div>
+          <label>Username:</label>
+          <input id="username" type="text" name="username" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input id="password" type="password" name="password" />
+        </div>
+        <div>
+          <input type="button" value="Login" onClick={
+            () => {
+              let username = document.getElementById('username').value;
+              let password = document.getElementById('password').value;
+              props.login({ username, password });
+              document.getElementById('username').value = '';
+              document.getElementById('password').value = '';
+            }
+          } />
+        </div>
+    </form>
+    <p>
+      <a onClick={() => { props.changeView('signup')}}>Create an Account &rarr;</a>
+    </p>
+  </div>
+);
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ changeView, login }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(Login);
