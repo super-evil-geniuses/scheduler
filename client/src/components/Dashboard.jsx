51de1c39d270c5b3e8aab17bad4bd32a4b7a7f5e
root <t.hoshman@gmail.com>
2017-11-24 17:47:17 -0800

    bringing over new files

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import EmployeeEditor from '../containers/EmployeeEditor.jsx';
+import ScheduleEditor from '../containers/ScheduleEditor.jsx';
+import ScheduleGenerator from '../containers/ScheduleGenerator.jsx';
+import ScheduleActual from './ScheduleActual.jsx';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: 'scheduleEditor'
+    }
+  }
+  
+  render() {
+    let editorView;
+    if(this.state.currentView === 'employeeEditor') {
+      editorView = <EmployeeEditor />
+    } else {
+      editorView = <ScheduleEditor />
+    }
+
+    return (
+      <div className="dashboard-container">
+        <div className="ratio-col-4 major-component">
+          <div className="component-block">
+            <div className="editor-header">
+              <div className="container clear-fix">
+                <div className="ratio-col-2 editor-tab clickable" onClick={() => { this.setState({currentView: 'employeeEditor' })}}>Employees</div>
+                <div className="ratio-col-2 editor-tab clickable" onClick={() => { this.setState({currentView: 'scheduleEditor' })}}>Schedule</div>
+              </div>
+            </div>
+          {editorView}
+          </div>
+        </div>
+        <div className="ratio-col-4-3 major-component">
+          <div className="component-block">
+            <ScheduleGenerator />
+            <ScheduleActual />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
