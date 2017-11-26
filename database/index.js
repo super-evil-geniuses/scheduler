120ed88ee7313d43a13b10a993896f5bbdcc8f7b
root <t.hoshman@gmail.com>
2017-11-26 12:16:45 -0800

    trying something else

--- database/index.js
+++ database/index.js
@@ -2,12 +2,10 @@ const Sequelize = require('sequelize');
 const config = require('./config.js');
 require('dotenv').config();
 
-console.log('config vars', Boolean(process.env.DATABASE_URL))
+
 if (process.env.DATABASE_URL) {
-	console.log('IF IS RUNNING..............')
-  const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});	
+  const sequelize = new Sequelize(process.env.DATABASE_URL);	
 } else {
-	console.log('ELSE IS RUNNING..............')
   const sequelize = new Sequelize(process.env.DB_NAME || 'shiftly', process.env.DB_USER || 'postgres', process.env.DB_PASS || null, { host: process.env.DB_HOST || 'localhost', dialect: 'postgres' });
 }
 
