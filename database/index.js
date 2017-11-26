ff36b522abb8445a2742f7502f4fc83749a54641
root <t.hoshman@gmail.com>
2017-11-26 11:18:58 -0800

    added a url route

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('./config.js');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'shiftly', process.env.DB_USER || 'postgres', process.env.DB_PASS || null, { host: process.env.DB_HOST || 'localhost', dialect: 'postgres' });
+const sequelize = new Sequelize(DATABASE_URL);
 
 const db = config(sequelize);
 
