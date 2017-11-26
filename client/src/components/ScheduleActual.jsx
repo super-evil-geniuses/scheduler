98818400bebf8d3aec0d60cb05de9cd01a053c4c
sofiegraham <sofiegraham@hotmail.com>
2017-11-26 14:59:47 -0800

    Update calendar to show selectedWeek schedule

--- client/src/components/ScheduleActual.jsx
+++ client/src/components/ScheduleActual.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'underscore';
-import { connect } from 'react-redux';
 import EmployeeSchedule from './EmployeeSchedule.jsx';
 
 
@@ -25,7 +24,7 @@ const ScheduleActual = (props) => {
         <div className="ratio-col-8 schedule-block ">Sun</div>
         {morningEvenings}
       </div>
-        {props.schedules.map((sched,idx) => {
+        {props.selectedWeekActualSchedule.map((sched,idx) => {
           return <EmployeeSchedule key={`${sched.name}${idx}`} schedule={sched} />;
         })}
     </div>
@@ -33,62 +32,4 @@ const ScheduleActual = (props) => {
   
 }
 
-const mapStateToProps = (state) => {
-	let schedules = {};
-  let scheduleArr = [];
-  //let dayPartsMap = {};
-  if (state.scheduleActual) {
-    state.scheduleActual.forEach( (e) => {
-      if(e.user_id === null) {
-        schedules['HOUSE'] = schedules['HOUSE'] || [];
-        schedules['HOUSE'].push(e.day_part_id);
-      } else {
-        schedules[e.user_id] = schedules[e.user_id] || [];
-        schedules[e.user_id].push(e.day_part_id);
-      }
-    });
-    
-    for (const sched in schedules) {
-      let schedObj = {}
-      if (sched === 'HOUSE') {
-        schedObj.name = 'HOUSE';
-        schedObj.schedule = schedules[sched];
-      } else {
-        schedObj.name = state.users.filter( (user) => {
-          return user.id == sched;
-        })[0].name
-
-        schedObj.schedule = schedules[sched];
-      }
-
-      scheduleArr.push(schedObj);
-    }
-  }
-  
-  // if(state.dayParts) {
-  //   let forwardFacingDayParts = [
-  //     null,
-  //     'Monday AM',
-  //     'Monday PM',
-  //     'Tuesday AM',
-  //     'Tuesday PM',
-  //     'Wednesday AM',
-  //     'Wednesday PM',
-  //     'Thursday AM',
-  //     'Thursday PM',
-  //     'Friday AM',
-  //     'Friday PM',
-  //     'Saturday AM',
-  //     'Saturday PM',
-  //     'Sunday AM',
-  //     'Sunday PM',
-  //   ];
-  //   state.dayParts.forEach((dayPart) => {
-  //     dayPartsMap[dayPart.id] = forwardFacingDayParts[dayPart.id];
-  //   })
-  // }
-
-  return { schedules: scheduleArr };
-}
-
-export default connect(mapStateToProps)(ScheduleActual);
+export default ScheduleActual;
