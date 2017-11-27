bacd1c50b95d138a745ea708e0d730fd9b97504d
root <t.hoshman@gmail.com>
2017-11-26 16:47:46 -0800

    removed merge conflicts

--- test/backendSpec.js
+++ test/backendSpec.js
@@ -13,11 +13,8 @@ const app = require('../server/app.js');
 const schema = require('../database/config.js');
 const algo = require('../helpers/algo.js');
 const utils = require('../helpers/index.js');
-<<<<<<< HEAD
 // const dummyData = require('../database/example-data/dummyData.js');
-=======
-// const dummyData = require('../database/example-data/dummyData.js');\
->>>>>>> login-data
+
 const port = process.env.PORT || 8080;
 
 describe('Shiftly Backend Test Spec', () => {
@@ -39,10 +36,7 @@ describe('Shiftly Backend Test Spec', () => {
     db.Day_Part.hasMany(db.Actual_Schedule, { as: 'actual_schedule' });
     db.Day_Part.hasMany(db.Needed_Employee, { as: 'needed_employee' });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> login-data
     const dayParts = [
       'monA', 'monP', 
       'tuesA', 'tuesP', 
