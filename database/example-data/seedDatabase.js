44d179829f2eb370c326fdcf21537126aec06e21
Katherine Manning <katm201@gmail.com>
2017-11-29 10:05:35 -0800

    Revert "Ref/remote db"

--- database/example-data/seedDatabase.js
+++ database/example-data/seedDatabase.js
@@ -3,72 +3,72 @@ const db = require('../../database');
 const dummyData = require('./dummyData');
 
 // Saves the week start date and the corresponding schedule id
-const saveSchedule = (weekStart) => {
-  return db.Schedule.create({ monday_dates: weekStart.monday_dates });
+let saveSchedule = (weekStart) => {
+	return db.Schedule.create({ monday_dates: weekStart.monday_dates });
 };
 
 // Saves one user
-const saveUser = (user) => {
-  return db.User.create({ name: user.name, role: user.role, password: user.password });
+let saveUser = (user) => {
+	return db.User.create({ name: user.name, role: user.role, password: user.password});
 };
 
 // Saves the schedule template
 let saveScheduleTemplate = (temp) => {
-  return Promise.each(temp, (day) => {
-    return db.Schedule.find({ where: { monday_dates: day.monday_date } })
-      .then((result) => {
-        let schedule_id = result.id;
-        return db.Day_Part.find({ where: { name: day.day_part } })
-          .then((result) => {
-            let day_part_id = result.id;
-            return db.Needed_Employee.create({
-              employees_needed: day.employees_needed,
-              schedule_id,
-              day_part_id,
-            });
-          });
-      });
-  });
+	return Promise.each(temp, (day) => {
+		return db.Schedule.find({ where: { monday_dates: day.monday_date } })
+			.then((result) => {
+				let schedule_id = result.id;
+				return db.Day_Part.find({ where: { name: day.day_part } })
+					.then((result) => {
+						let day_part_id = result.id;
+						return db.Needed_Employee.create({ 
+							employees_needed: day.employees_needed,
+							schedule_id: schedule_id,
+							day_part_id: day_part_id
+						});
+					})
+			})
+	});
 }
 
 // Saves availability for one employee
 let saveEmployeeAvailability = (avail) => {
-  return Promise.each(avail, (employeeAvail) => {
-    return db.User.find({ where: { name: employeeAvail.name } })
-      .then((result) => {
-        let user_id = result.id;
-        return db.Day_Part.find({ where: { name: employeeAvail.day_part } })
-          .then((result) => {
-            let day_part_id = result.id;
-            return db.Employee_Availability.create({
-              is_available: employeeAvail.is_available,
-              user_id,
-              day_part_id,
-            });
-          });
-      });
-  });
+	return Promise.each(avail, (employeeAvail) => {
+		return db.User.find({ where: {name: employeeAvail.name} })
+			.then((result) => {
+				let user_id = result.id;
+				return db.Day_Part.find({ where: {name: employeeAvail.day_part} })
+					.then((result) => {
+						let day_part_id = result.id;
+						return db.Employee_Availability.create({
+							is_available: employeeAvail.is_available,
+							user_id: user_id,
+							day_part_id: day_part_id
+						});
+					});
+			});
+	});
 }
 
 // initializes the database with dummy data
 let initialize = () => {
-  return saveSchedule(dummyData.weekStart)
-    .then(() => {
-      return Promise.each(dummyData.users, (user) => {
-        saveUser(user);
-      });	
-    })
-    .then(() => {
-      return saveScheduleTemplate(dummyData.temp1);
-    })
-    .then(() => {
-      return Promise.each(dummyData.avails, (avail) => {
-        return saveEmployeeAvailability(avail);
-      });
-    })
-    .then(() => {
-      db.sequelize.close();
-    });
-};
+	return saveSchedule(dummyData.weekStart)
+		.then(() => {
+			return Promise.each(dummyData.users, (user) => {
+				saveUser(user);
+			});	
+		})
+		.then(() => {
+			return saveScheduleTemplate(dummyData.temp1);
+		})
+		.then(() => {
+			return Promise.each(dummyData.avails, (avail) => {
+				return saveEmployeeAvailability(avail);
+			});
+		})
+		.then(() => {
+			db.sequelize.close();
+		})
+}
 
 initialize();
