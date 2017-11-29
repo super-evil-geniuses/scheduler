38844b8582b267a9f71e36f63d73cdd93909c67e
Danny Welstad <dannywelstad@gmail.com>
2017-11-28 16:37:32 -0800

    airbnb linter errors

--- database/index.js
+++ database/index.js
@@ -4,49 +4,59 @@ const Promise = require('bluebird');
 require('dotenv').config();
 
 
-const sequelize = process.env.DATABASE_URL ? 
-new Sequelize(process.env.DATABASE_URL) : 
-new Sequelize(process.env.DB_NAME || 'shiftly', process.env.DB_USER || 'postgres', process.env.DB_PASS || null, { host: process.env.DB_HOST || 'localhost', dialect: 'postgres' });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+});
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const db = config(sequelize);
 
-db.User.hasMany(db.Session, {as: 'session'});
+db.User.hasMany(db.Session, { as: 'session' });
 
 // One-to-Many Relationships
-db.User.hasMany(db.Actual_Schedule, { as: 'actual_schedule'});
+db.User.hasMany(db.Actual_Schedule, { as: 'actual_schedule' });
 db.User.hasMany(db.Employee_Availability, { as: 'employee_availability' });
 
-db.Schedule.hasMany(db.Actual_Schedule, { as: 'actual_schedule'});
+db.Schedule.hasMany(db.Actual_Schedule, { as: 'actual_schedule' });
 db.Schedule.hasMany(db.Needed_Employee, { as: 'needed_employee' });
 
 db.Day_Part.hasMany(db.Employee_Availability, { as: 'employee_availability' });
 db.Day_Part.hasMany(db.Actual_Schedule, { as: 'actual_schedule' });
 db.Day_Part.hasMany(db.Needed_Employee, { as: 'needed_employee' });
 
-// drops all table, just put it in so that it doesn't give an error for creating the same table everytime during dev
+// drops all table, just put it in so that it doesn't give an error
+// for creating the same table everytime during dev
+
 db.User.sync()
-	.then(() => {
-		return db.Schedule.sync();	
-	})
-	.then(() => {
-		return db.Day_Part.sync();
-	})
-	.then(() => {
-		return db.Employee_Availability.sync();
-	})
-	.then(() => {
-		return db.Actual_Schedule.sync();
-	})
-	.then(() => {
-		return db.Needed_Employee.sync();
-	})
-	.then(() => {
-		return db.Session.sync();
-	})
-	.then(() => {
-		return saveDayParts(dayParts);
-	})
+  .then(() => {
+    return db.Schedule.sync();	
+  })
+  .then(() => {
+    return db.Day_Part.sync();
+  })
+  .then(() => {
+    return db.Employee_Availability.sync();
+  })
+  .then(() => {
+    return db.Actual_Schedule.sync();
+  })
+  .then(() => {
+    return db.Needed_Employee.sync();
+  })
+  .then(() => {
+    return db.Session.sync();
+  })
+  .then(() => {
+    return saveDayParts(dayParts);
+  });
 
 const dayParts = [
   'monA', 'monP', 
