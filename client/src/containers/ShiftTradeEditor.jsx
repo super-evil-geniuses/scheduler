dc587e127f12fc4fa1087c54a7c9d19d85a36ad6
Katherine Manning <katm201@gmail.com>
2017-12-02 10:42:54 -0800

    (feat) update trade formatting with schedule info

--- client/src/containers/ShiftTradeEditor.jsx
+++ client/src/containers/ShiftTradeEditor.jsx
@@ -18,15 +18,60 @@ class ShiftTradeEditor extends Component {
 };
 
 const mapStateToProps = (state) => {
-  const { userRole, trades } = state;
+  const { userRole, trades, scheduleActual, scheduleDates, dayParts } = state;
+
+  const shiftNames = {
+    monA: 'Monday AM',
+    monP: 'Monday PM',
+    tuesA: 'Tuesday AM',
+    tuesP: 'Tuesday PM',
+    wedsA: 'Wednesday AM',
+    wedsP: 'Wednesday PM',
+    thursA: 'Thursday AM',
+    thursP: 'Thursday PM',
+    friA: 'Friday AM',
+    friP: 'Friday PM',
+    satA: 'Saturday AM',
+    sunA: 'Sunday AM',
+    sunP: 'Sunday PM',
+  }
+
+  const formattedTrades = trades.map((trade) => {
+    const actualSchedule = scheduleActual.filter((schedule) => {
+      return schedule.id === trade.actual_schedule_id;
+    });
+    const scheduleDate = scheduleDates.filter((scheduleDate) => {
+      return scheduleDate.id === actualSchedule.schedule_id;
+    });
+    const dayPart = dayParts.filter((dayPart) => {
+      return dayPart.id === actualSchedule.day_part_id;
+    })
+
+    const formattedTrade = {};
+    formattedTrade.id = trade.id;
+    formattedTrade.status = trade.status;
+    formattedTrade.requester = {
+      userId: trade.id,
+      name: trade.name,
+    };
+    formattedTrade.scheduleInfo = {
+      scheduleId: actualSchedule.id,
+      weekOf: scheduleDate.monday_dates,
+      shift: shiftNames[dayPart.name],
+    };
+
+    return formattedTrade;
+  })
+
   return {
     userRole,
-    trades,
+    trades: formattedTrades,
   };
 };
 
 ShiftTradeEditor.propTypes = {
   userRole: PropTypes.string.isRequired,
+  trades: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default connect(mapStateToProps)(ShiftTradeEditor);
